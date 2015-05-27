bc260fff019e33a3264205935d443fdb69d9ab93
11118271 <aagreen09@gmail.com>
2015-05-27 17:44:47 +0200

    Update Homework_12.php

--- Homework_12.php
+++ Homework_12.php
@@ -3,6 +3,7 @@
   <title>Homework_12 PHP Program</title>
  </head>
  <body>
- <?php echo '<p>Thabiso 11205467</p>'; ?> 
+ <?php echo '<p>Thabiso 11205467</p>';
+       echo '<p>Ayabonga 11118271</p>'; ?> 
  </body>
 </html>
